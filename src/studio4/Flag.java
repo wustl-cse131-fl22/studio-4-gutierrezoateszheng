6c6236a3401ae9862b85689d0f3bfc42f1092baa
sofiagutierrez <sofiagutierrez@sofias-mbp.dhcp.wustl.edu>
2022-10-13 14:06:59 -0500

    studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,22 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(220, 208, 255);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.2);
+		
+		StdDraw.setPenColor(246,247,244);
+		StdDraw.filledCircle(0.499, 0.45, 0.08);
+		
+		StdDraw.setPenColor(208,240,192);
+		StdDraw.filledCircle(0.43, 0.54, 0.08);
+		
+		StdDraw.setPenColor(100,136,234);
+		StdDraw.filledCircle(0.56, 0.54, 0.08);
+		
+	
+		
+		
+		
+		
 	}
 }
